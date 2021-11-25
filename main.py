fe303dec7c5aabd8c753220395b7f14d849b0f2d
unknown <sumitlala99@gmail.com>
2021-11-25 10:21:09 +0530

    third commit

--- main.py
+++ main.py
@@ -5,13 +5,10 @@ from wsgiref import simple_server
 
 from flask import Flask, session, request, Response, jsonify
 
-
-
 import atexit
 import uuid
 import os
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -19,10 +16,17 @@ def index():
 
 #port = int(os.getenv("PORT", 5000))
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     #host = '0.0.0.0'
-    app.run()
+    #app.run()
     #httpd = simple_server.make_server(host=host, port=5000, app=app)
     #httpd = simple_server.make_server(host='127.0.0.1', port=5000, app=app)
     # print("Serving on %s %d" % (host, port))
     #httpd.serve_forever()
+port = int(os.getenv("PORT",5000))
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    #port = 5000
+    httpd = simple_server.make_server(host, port, app)
+    # print("Serving on %s %d" % (host, port))
+    httpd.serve_forever()
